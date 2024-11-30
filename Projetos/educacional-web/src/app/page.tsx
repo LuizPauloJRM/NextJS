96043a0d85030eaf7f65a1d79f4df43339ef8afa
LuizPauloJRM <luizpaulomedeiros77@hotmail.com>
2024-11-30 14:11:01 -0300

    criação das section

--- Projetos/educacional-web/src/app/page.tsx
+++ Projetos/educacional-web/src/app/page.tsx
@@ -6,9 +6,34 @@ import IntroSection from './components/intro-section';
 // import InvestmentSection from './components/investment-section';
 // import FaqSection from './components/faq-section';
 
+
 const Home = () => {
+  
   return (
+    
     <main className='pt-20 md:pt-18'>
+      <section id ='intro' className='space-y-6 py-0 nd:py-12'>
+        <div className='container'>
+
+        </div>
+        <h1 className='text-primary'>...</h1>
+      </section>
+      <section id='about'>
+        <h1 className='text-primary'>Sobre</h1>
+
+      </section>
+      <section id='feature'>
+        <h1 className='text-primary'>...</h1>
+
+      </section>
+      <section id='investment'>
+        <h1 className='text-primary'>...</h1>
+
+      </section>
+      <section id='faq'>
+        <h1 className='text-primary'>faq</h1>
+
+      </section>
       <IntroSection />
       { /* <AboutSection /> */ }
       { /* <FeatureSection /> */ }
